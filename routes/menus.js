ab7490fc76023c9b09a3d61e03a51572c16ef015
shadowM8 <anton2014lenovo@gmail.com>
2019-04-21 16:10:57 +0700

    end point users and menus

---
+++ routes/menus.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+const {getMenusList, findById, createMenu} = require('../helpers/firebase/menus')
+
+/* GET menus listing. */
+router.get('/', function (req, res, next) {
+  getMenusList()
+    .then(menus => {
+      res.status(200).json(menus)    
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+});
+
+/* GET menu Id listing. */
+router.get('/:menuId', function(req,res,next) {
+  findById(req.params.menuId)
+    .then(menu => {
+      res.status(200).json(menu)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+/* POST new menus listing. */
+router.post('/', function (req, res, next) {
+  createMenu(req.body.image,req.body.name,req.body.max,req.body.price,req.body.time)
+    .then(menu => {
+      res.status(201).json(menu)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+})
+
+/* EDIT menus  */
+router.put('/:menuId', function (req, res, next) {
+  res.send('...')
+})
+
+/* DELETE menus  */
+router.delete('/:menuId', function (req, res, next) {
+  res.send('...')
+})
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,38 +1,40 @@
 const express = require('express');
 const router = express.Router();
-const {db, auth} = require('../models/firebase')
+const {dbUsers, auth} = require('../helpers/firebase/index')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register', function(req,res,next) {
-  auth.createUserWithEmailAndPassword(req.body.email,req.body.password)
-  .then(user => {
-    res.status(201).json({
-      user,
-      token : auth.currentUser.getIdToken()
+router.post('/register', function (req, res, next) {
+  auth.createUserWithEmailAndPassword(req.body.email, req.body.password)
+    .then(user => {
+      console.log(user.user.uid)
+      return dbUsers.doc(user.user.uid).set({
+        name: req.body.name
+      })
     })
-
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+    .then(() => {
+      res.status(201).json({message: 'user created'})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 })
 
-router.post('/login', function(req,res,next) {
+router.post('/login', function (req, res, next) {
   auth.signInWithEmailAndPassword(req.body.email, req.body.password)
-  .then(user => {
-    console.log(user)
-    console.log('token',auth.currentUser.getIdToken())
-    res.status(200).json(auth.currentUser.getIdToken())
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+    .then(user => {
+      console.log(user.user)
+      res.status(200).json({ uid: user.user.uid})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 })
+
 
 module.exports = router;
