af82936c1f968e801acbd881f3607718ad20a30c
shadowM8 <anton2014lenovo@gmail.com>
2019-04-22 10:44:48 +0700

    login fitur

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,56 @@ router.get('/', function (req, res, next) {
 });
 
 router.post('/register', function (req, res, next) {
+  
   auth.createUserWithEmailAndPassword(req.body.email, req.body.password)
     .then(user => {
       console.log(user.user.uid)
+      console.log(req.body)
       return dbUsers.doc(user.user.uid).set({
-        name: req.body.name
+        name: req.body.name,
+        uid: user.user.uid,
+        role: +req.body.role,
+        table: 0,
+        arrival: new Date()
       })
     })
     .then(() => {
-      res.status(201).json({message: 'user created'})
+      auth.onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+          dbUsers.doc(firebaseUser.uid).get()
+          .then(user => {
+            res.status(201).json(user.data())
+          })
+          .catch(err => {
+            throw new Error(err.message)
+          })
+        }
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+    
+})
+
+router.post('/login', function (req, res, next) {
+  auth.signInWithEmailAndPassword(req.body.email, req.body.password)
+    .then(user => {
+      // console.log(user.user)
+      // res.status(200).json({ uid: user.user.uid})
+      auth.onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+          // res.status(200).json(firebaseUser)
+          dbUsers.doc(firebaseUser.uid).get()
+          .then(user => {
+            res.status(200).json(user.data())
+          })
+          .catch(err => {
+            throw new Error(err.message)
+          })
+        }
+      })
     })
     .catch(err => {
       console.log(err)
@@ -24,16 +65,23 @@ router.post('/register', function (req, res, next) {
     })
 })
 
-router.post('/login', function (req, res, next) {
-  auth.signInWithEmailAndPassword(req.body.email, req.body.password)
-    .then(user => {
-      console.log(user.user)
-      res.status(200).json({ uid: user.user.uid})
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json(err)
-    })
+router.get('/logout', function(req,res,next) {
+  auth.onAuthStateChanged(firebaseUser => {
+    if(firebaseUser) {
+      auth.signOut()
+        .then(() => {
+          res.status(200).json({message:`user ${firebaseUser.email} successs log out`})
+        })
+        .catch(err => {
+          console.log(err)
+          res.status(500).json(err)
+        })
+    }
+    else {
+      res.status(200).json({message:'no one log in'})
+    }
+  })
+  
 })
 
 
