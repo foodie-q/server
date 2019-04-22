9f83693fcd0da117e53c876ca24196d66dbf2942
Mahbub <prof.bub@live.com>
2019-04-22 20:42:42 +0700

    added qrcode

--- routes/qr.js
+++ routes/qr.js
@@ -1,9 +1,24 @@
 const router = (require('express')).Router();
 const qrcode = require('qrcode');
+const {dbUsers} = require('../helpers/firebase');
 
 router
+  .post('/', ({body: {userId, table}}, res) => {
+    dbUsers
+      .doc(userId)
+      .get()
+      .then(async (doc) => {
+        if (!!+table && await doc.data()) {
+          res
+            .json({valid: 1})
+        } else {
+          res
+            .json({valid: 0})
+        }
+      })
+  })
   .get('/', (req, res) => {
-    let keyRandom = `qo test`;
+    let keyRandom = `1`;
     qrcode.toDataURL(keyRandom, {errorCorrectionLevel: "H", type: "image/png"}, (err, data) => {
       if (err) {
         console.error(err);
