cbfcd1b66b85775968fe95b2e24295d56af21dc2
Mahbub <prof.bub@live.com>
2019-04-25 14:58:48 +0700

    change api key

--- helpers/firebase/index.js
+++ helpers/firebase/index.js
@@ -6,7 +6,7 @@ require('firebase/storage');
 require('dotenv').config()
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyCmko1vka8wbPXEKS39mhMZyfJqDWrkGlo",
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.FIREBASE_DATABASE_URL,
   projectId: "final-project-anton-zul-felix",
